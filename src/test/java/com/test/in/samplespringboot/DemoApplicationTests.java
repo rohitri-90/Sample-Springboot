589b2ac5ff1f77b5d7eb0e8ab21a6f7931a533e2
rohitri-90 <39183792+rohitri-90@users.noreply.github.com>
2019-09-05 20:28:37 +0530

    Initial commit

---
+++ src/test/java/com/test/in/samplespringboot/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.in.samplespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
